e27e5be02b7988bd3d7ef84ce4841f9b944e3ad8
Henri Heimonen <henri.heimonen@tuni.fi>
2022-12-03 16:26:28 +0200

    added tests for memoize e.g. using hash resolver

--- test/test_memoize.js
+++ test/test_memoize.js
@@ -1,22 +1,35 @@
 import chai from 'chai';
 import memoize from '../src/memoize.js';
 import type from 'type-detect';
-
-// simple test function to return values of an object in an array
-// const values = (obj) => {
-//     return Object.values(obj);
-// } 
+import hash from 'object-hash';
 
 describe('Memoize.js', () => {
     // create test objects as per documentation
     const object = { 'a': 1, 'b': 2 }
     const other = { 'c': 3, 'd': 4 }
+    const third = { 'x': 10, 'y': 11 }
 
     const values = memoize(Object.values);
 
     it('should throw Expected a function if first arg is not function', async () => {
         try {
             memoize(object);   
+        } catch (error) {
+            chai.expect(error.message).to.equal('Expected a function'); 
+        }   
+    });
+
+    it('should throw Expected a function if first arg is not function and second argument is null', async () => {
+        try {
+            memoize(object, null);   
+        } catch (error) {
+            chai.expect(error.message).to.equal('Expected a function'); 
+        }   
+    });
+
+    it('should throw Expected a function if first arg is not function and second argument is not null', async () => {
+        try {
+            memoize(object, hash);   
         } catch (error) {
             chai.expect(error.message).to.equal('Expected a function'); 
         }   
@@ -32,6 +45,16 @@ describe('Memoize.js', () => {
 
     it('should return values [1,2] for object', async () => {
         chai.expect(values(object)).to.eql([1,2]);
+    });
+
+    it('should return values [1,2] for object with resolver provided', async () => {
+        chai.expect(values(object)).to.eql([1,2]);
+    });
+
+    it('should return right values for object when using its hash as key', async () => {
+        const values2 = memoize(Object.values, hash);
+        chai.expect(values2(third)).to.eql([10,11]);
+        chai.expect(values2.cache.get(hash(third))).to.eql([10,11]);
     });
 
     it('should still return [1,2] for object if using memoizer even if value changed', async () => {
