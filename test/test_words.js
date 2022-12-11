f0aa1f031fa4082a5b70d9930a85cf31ec8d0006
empokki <87477709+empokki@users.noreply.github.com>
2022-12-11 18:28:54 +0200

    added few basic tests for words.js

--- test/test_words.js
+++ test/test_words.js
@@ -2,6 +2,43 @@ import words from '../src/words.js'
 import chai from 'chai'
 const expect = chai.expect
 
+/**
+ * Splits `string` into an array of its words.
+ *
+ * @since 3.0.0
+ * @category String
+ * @param {string} [string=''] The string to inspect.
+ * @param {RegExp|string} [pattern] The pattern to match words.
+ * @returns {Array} Returns the words of `string`.
+ * @example
+ *
+ * words('fred, barney, & pebbles')
+ * // => ['fred', 'barney', 'pebbles']
+ *
+ * words('fred, barney, & pebbles', /[^, ]+/g)
+ * // => ['fred', 'barney', '&', 'pebbles']
+ */
+
 describe('Words.js', () => {
-    
+    const stringWithAsciiWords = 'Pizza, Burger & Fries'
+    const stringWithUnicodeWords = '🍕, 🍔 & 🍟'
+    const re = /[^, ]+/g;
+
+    it('should return words of string when calling with Ascii words', () => {
+        expect(words(stringWithAsciiWords)).to.eql(['Pizza', 'Burger', 'Fries']);
+    });
+
+    it('should return words of string when calling with Unicode words', () => {
+        expect(words(stringWithUnicodeWords)).to.eql(['🍕', '🍔', '🍟']);
+    });
+
+    it('should return words of string that match given pattern (ascii words)', () => {
+        expect(words(stringWithAsciiWords, re)).to.eql(['Pizza', 'Burger', '&', 'Fries']);
+    });
+
+    it('should return words of string that match given pattern (unicode words)', () => {
+        expect(words(stringWithUnicodeWords, re)).to.eql(['🍕', '🍔', '&', '🍟']);
+    });
+
+
 });
